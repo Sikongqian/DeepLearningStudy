00528ae3327e5fe73e4ab438e81d091f5f978577
Sikong Qian <82042892+2513362477@users.noreply.github.com>
2022-07-20 17:39:07 +0800

    add in lecture 7/20

--- Lecture2/MNISTTrain.py
+++ Lecture2/MNISTTrain.py
@@ -2,6 +2,9 @@
 from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+print(train_images.ndim)
+print(train_images.shape)
+print(train_images.dtype)
 # 网络架构
 from keras import models
 from keras import layers
---
+++ Lecture2/TensorTest.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#一维张量
+x = np.array(12)
+print(x.ndim)
+
+#多维张量
+x = np.array([[[5,78],[5,90]],[[5,78],[5,90]]])
+print(x)
